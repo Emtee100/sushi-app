dbf7a902b823746ef6995fddf939cabf6b8a0806
Emtee100 <mt9510037@gmail.com>
2023-01-27 14:14:35 +0300

    Categories finished

--- sushi_app/lib/pages/home_page.dart
+++ sushi_app/lib/pages/home_page.dart
@@ -17,111 +17,127 @@ class _HomePageState extends State<HomePage> {
     {"icon": FontAwesomeIcons.shrimp, "name": "Shrimp"},
   ];
 
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey.shade200,
         body: SafeArea(
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          // options button
-          SizedBox(height: 35),
+            child: SingleChildScrollView(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            // options button
+            SizedBox(height: 35),
 
-          Padding(
-            padding: const EdgeInsets.only(left: 25),
-            child: GestureDetector(
-              onTap: (() {
-                print("Options clicked");
-              }),
-              child: Container(
-                padding: EdgeInsets.all(14),
-                decoration:
-                    BoxDecoration(color: Colors.white, shape: BoxShape.circle),
-                child: FaIcon(FontAwesomeIcons.bars),
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: GestureDetector(
+                onTap: (() {
+                  print("Options clicked");
+                }),
+                child: Container(
+                  padding: EdgeInsets.all(14),
+                  decoration: BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
+                  child: FaIcon(FontAwesomeIcons.bars),
+                ),
               ),
             ),
-          ),
 
-          SizedBox(height: 25),
-          // greeting
+            SizedBox(height: 25),
+            // greeting
 
-          Padding(
-            padding: const EdgeInsets.only(left: 25),
-            child: Text(
-              '👋Hi, Angel!',
-              style: GoogleFonts.montserrat(
-                  fontSize: 20, fontWeight: FontWeight.w600),
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Text(
+                '👋Hi, Angel!',
+                style: GoogleFonts.montserrat(
+                    fontSize: 20, fontWeight: FontWeight.w600),
+              ),
             ),
-          ),
 
-          SizedBox(height: 25),
+            SizedBox(height: 25),
 
-          // main greeting
-          Padding(
-            padding: EdgeInsets.only(left: 25),
-            child: Text(
-              'What is your favorite sushi?',
-              style: GoogleFonts.montserrat(
-                  fontSize: 35, fontWeight: FontWeight.w600),
+            // main greeting
+            Padding(
+              padding: EdgeInsets.only(left: 25),
+              child: Text(
+                'What is your favorite sushi?',
+                style: GoogleFonts.montserrat(
+                    fontSize: 35, fontWeight: FontWeight.w600),
+              ),
             ),
-          ),
-          SizedBox(height: 25),
+            SizedBox(height: 25),
 
-          // Search bar
-          searchBar(),
+            // Search bar
+            searchBar(),
 
-          SizedBox(height: 25),
-          // categories
-          Padding(
-            padding: const EdgeInsets.only(left: 25, right: 25),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Categories",
-                  style: GoogleFonts.montserrat(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 25,
+            SizedBox(height: 25),
+            // categories
+            Padding(
+              padding: const EdgeInsets.only(left: 25, right: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Categories",
+                    style: GoogleFonts.montserrat(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 25,
+                    ),
                   ),
-                ),
-                TextButton(
-                    onPressed: () => print("See all"),
-                    child: Text(
-                      "See all",
-                      style:
-                          GoogleFonts.montserrat(fontWeight: FontWeight.w400),
-                    ))
-              ],
+                  TextButton(
+                      onPressed: () => print("See all"),
+                      child: Text(
+                        "See all",
+                        style:
+                            GoogleFonts.montserrat(fontWeight: FontWeight.w400),
+                      ))
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 15),
+            SizedBox(height: 15),
 
-          Container(
-            margin: EdgeInsets.only(left: 20, right: 20),
-              height: 75.0,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: categories.length,
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.fromLTRB(8, 12, 8, 8),
-                          margin: EdgeInsets.symmetric(horizontal: 12),
-                          child: FaIcon(
-                            categories[index]["icon"],
+            Container(
+                margin: EdgeInsets.only(left: 20, right: 20),
+                height: 75.0,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: categories.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                _selectedIndex = index;
+                              });
+                              print(_selectedIndex);
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.fromLTRB(8, 12, 8, 8),
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 12),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: _selectedIndex == index
+                                    ? Color.fromARGB(255, 31, 39, 53)
+                                    : Colors.white,
+                              ),
+                              child: FaIcon(
+                                categories[index]["icon"],
+                                color: _selectedIndex == index?Colors.white: Color.fromARGB(255, 31, 39, 53)
+                              ),
+                            ),
                           ),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(categories[index]["name"]),
-                      ],
-                    );
-                  })),
-                  
-          //top Sushi
-        ])));
+                          Text(categories[index]["name"]),
+                        ],
+                      );
+                    })),
+
+            //top Sushi
+          ]),
+        )));
   }
 }
--- sushi_app/lib/pages/util/search_bar.dart
+++ sushi_app/lib/pages/util/search_bar.dart
@@ -7,28 +7,28 @@ class searchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25),
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.grey.shade300,
-                          blurRadius: 8,
-                          spreadRadius: 3,
-                          offset: Offset(1, 8))
-                    ],
-                    color: Colors.white,
-                  ),
-                  child: TextField(
-                    style: GoogleFonts.montserrat(),
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.search),
-                        hintText: "Search your sushi",
-                        hintStyle: GoogleFonts.montserrat(),
-                        border: InputBorder.none),
-                  ),
-                ));
+        padding: EdgeInsets.symmetric(horizontal: 25),
+        child:Container(
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.shade300,
+                  blurRadius: 8,
+                  spreadRadius: 3,
+                  offset: Offset(1, 8))
+            ],
+            color: Colors.white,
+          ),
+          child: TextField(
+            style: GoogleFonts.montserrat(),
+            decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: "Search your sushi",
+                hintStyle: GoogleFonts.montserrat(),
+                border: InputBorder.none),
+          ),
+        ));
   }
 }
